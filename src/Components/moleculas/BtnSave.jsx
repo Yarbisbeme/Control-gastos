8e907b49f9c9c9a3cbe9da6352f38b4103ec9c36
yarbisbeme <yarbisbeme@gmail.com>
2025-05-26 14:37:36 -0400

    creando componentes

---
+++ src/Components/moleculas/BtnSave.jsx
@@ -0,0 +1,12 @@
+
+import styled from "styled-components";
+export function BtnSave() {
+  return (
+    <Container>
+      <h1>BtnSave</h1>
+    </Container>
+  );
+}
+const Container =styled.div`
+  
+`
--- src/Components/templates/loginTemplate.jsx
+++ src/Components/templates/loginTemplate.jsx
@@ -1,12 +1,43 @@
-
+import { v, BtnSave } from "../../index"
 import styled from "styled-components";
 export function LoginTemplate() {
   return (
     <Container>
-      <h1>LoginTemplate</h1>
+        <div className="contentCard">
+            <span>Version 1.0</span>
+            <div className="contentImg">
+                <img src={v.logo} alt="" />
+            </div>
+            <Titulo>Cerdyn</Titulo>
+            <p className="frase">Toma el control de tus 💵gastos e 💰ingresos</p>
+            <ContainerBtn>
+              <BtnSave></BtnSave>
+            </ContainerBtn>
+        </div>
     </Container>
   );
 }
 const Container =styled.div`
-  
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+  .contentCard{
+    background-color: black;
+    border-radius: 20px;
+    gap: 30px;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    margin: 20px;
+  }
+`
+
+const Titulo = styled.span`
+  font-size: 5rem;
+  font-weight: 700;
+`;
+
+const ContainerBtn = styled.div`
+  display: flex;
+  justify-content: center;
 `
